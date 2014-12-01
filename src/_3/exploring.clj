db0edcf8f212685623b37e1f5f7654139cbfb256
Raymond Tay <raymondtay1974@gmail.com>
2014-12-01 12:48:41 +0800

    renamed and moved

--- src/_3/exploring.clj
+++ src/_3/exploring.clj
@@ -8,3 +8,11 @@
 ;; what i'm doing (above) is to find out how much
 ;; does (1 + 2 + .. + 10) equal to? not surprisingly, its 45
 
+;; Another example is the following, where we build
+;; a map on the fly (building ADTs on the fly isn't that cool
+;; anymore isn't it? rather it was cool when i first heard of 
+;; it when i started programming in 1997
+(reduce (fn [m v] (assoc m v (* v v))) {} [ 1 2 3 4])
+
+;; returns {4 16, 3 9, 2 4, 1 1}
+
